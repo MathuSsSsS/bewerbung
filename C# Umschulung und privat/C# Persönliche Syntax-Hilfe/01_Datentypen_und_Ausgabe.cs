e01dd97ab31c77ae671bcc21dfb50c1fa3c2a72c
MathuS_ <164481270+MathuSsSsS@users.noreply.github.com>
2024-06-24 09:32:08 +0200

    Add files via upload

---
+++ C# Umschulung und privat/C# Persönliche Syntax-Hilfe/01_Datentypen_und_Ausgabe.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20240325_C__Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Datentypen und deren Ausgabe mit Console.Writeline()
+            int x;              // Deklaration
+            x = 123;            // Initialisierung
+
+            int y = 456;        // Deklaration und Initialisierung
+
+            int z = x + y;      // Operation zweier Werte
+
+            int alter = 30;         // Ganzzahlen (Integer)
+            double height = 123.45; // Dezimalzahlen (Double)
+            bool alive = false;     // Boolische Zustände (true oder false)
+            char symbol = '@';      // Ein einziges Zeichen (Char)
+            String name = "Brudi";  // Zeichenkette (String)
+            
+            //Konstanten verhindern der Veränderung von Werten in einem laufendem Programm
+            const double PI = 3.14159265359;
+
+            Console.WriteLine("Hallo " + name);
+            Console.WriteLine("Du bist " + alter + "Jahre alt");
+            Console.WriteLine("Deine Höhe ist " + height + "cm");
+            Console.WriteLine("Bist du am Leben? " + alive);
+            Console.WriteLine("Symbol: " + symbol);
+            Console.WriteLine("PI: " + PI);
+
+            String benutzername = symbol + name;
+
+            Console.WriteLine("Dein Benutzername ist: " + benutzername);
+
+            //mehrere Variablen in einem String ausgeben
+            Console.WriteLine("Name: {0} und dein Alter: {1}", name, alter);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Umschulung und privat/C# Persönliche Syntax-Hilfe/05_string_methods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20240325_C__5_string_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Max Mustermann";
+            string telefonnummer = "0521 51823932";
+
+            // alle buchstaben in großbuchstaben umwandeln
+            name = name.ToUpper();
+            // alle buchstaben in kleinbuchstaben umwandeln
+            name = name.ToLower();
+
+            // in einem string bestimmte Zeichen mit einem anderen Zeichen ersetzen
+            telefonnummer = telefonnummer.Replace(" ", "/");
+            Console.WriteLine(telefonnummer);
+
+            // einem string einen anderen string hinzufügen
+            // 0 = Startposition, "Dr. " = hinzuzufügender String
+            String NameMitAkGrad = name.Insert(0, "Dr. ");
+            Console.WriteLine(NameMitAkGrad);
+
+            // Ausgabe der Länge eines Strings in Ganzzahl
+            Console.WriteLine(name.Length);
+
+            // Ausgabe eines bestimmten Zeichens an einer bestimmten Position
+            Console.WriteLine(name[2]);
+
+            // Ausgabe eines bestimmten Teils eines Strings
+            // Startposition, Anzahl der Zeichen
+            Console.WriteLine(name.Substring(4, 5));
+
+
+            // prüfen, ob ein string mit einem bestimmten string beginnt
+            bool startsWith = name.StartsWith("Max");
+            // prüfen, ob ein string mit einem bestimmten string endet
+            bool endsWith = name.EndsWith("mann");
+            // prüfen, ob ein string einen bestimmten string enthält
+            bool contains = name.Contains("Muster");
+            // prüfen, ob ein string leer ist
+            bool isEmpty = string.IsNullOrEmpty(name);
+            Console.WriteLine(String.Format("Starts with: {0}, Ends with: {1}, Contains: {2}, Is empty: {3}", startsWith, endsWith, contains, isEmpty));      
+            Console.ReadKey();
+        }
+    }
+}
